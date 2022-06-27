41d0553ba5262aea12afb56ccbf0f4bbf65240f1
PdskDev <prestadesk@gmail.com>
2022-06-27 19:05:56 +0200

    Last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditContactComponent } from './components/edit-contact/edit-contact.com
 import { ViewContactComponent } from './components/view-contact/view-contact.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TestCompComponent } from './test-comp/test-comp.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     ViewContactComponent,
     SpinnerComponent,
     PageNotFoundComponent,
+    TestCompComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
